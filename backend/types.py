e0c5db10054fddf30fc1ef6ae34385197b668290
Shyam Swaroop <30747788+cruxcode@users.noreply.github.com>
2022-11-11 10:31:10 +0530

    Update types.py

--- backend/types.py
+++ backend/types.py
@@ -1,5 +1,11 @@
 from typing import TypedDict
+from datetime import datetime
 
 class UniqueViewsDocument(TypedDict):
+    """
+    The data in GitHub gets lost after every 14 days, hence, we need to
+    record the date on which the data has been fetched.
+    """
+    date: datetime
     site: str
     unique_views: int
